a0b558775a54b1fa46da31966e76341f2f954dfb
GartzenDeHaes <103427029+GartzenDeHaes@users.noreply.github.com>
2023-04-20 11:16:18 -0700

    Documention

--- MirrorSplitScreen/Assets/ThirdParty/Portland/Unity/Behaviours/Context.cs
+++ MirrorSplitScreen/Assets/ThirdParty/Portland/Unity/Behaviours/Context.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 namespace Portland.Unity
 {
-	public enum NetworkLifecycleEventType
-	{
-		ClientStart,
-		ClientStop,
-	}
+	//public enum NetworkLifecycleEventType
+	//{
+	//	ClientStart,
+	//	ClientStop,
+	//}
 
 	public sealed class Context : MonoBehaviour
 	{
@@ -23,17 +23,19 @@ namespace Portland.Unity
 		public static readonly String10 MSG_MAP_INPUT = new String10("MAP_INPUT");
 		public static readonly String10 MSG_MINIMAP_ZOOM = new String10("MINIMAPZ");
 
+		/// <summary>Sent when the nework manager spawns the local player (LocalPlayerSpawner.cs). No arguments.</summary>
 		public static readonly String10 MSG_NET_SPAWNED = new String10("NET_SPN");
+		/// <summary>Sent when the nework manager calls OnStopLocalPlayer(). No arguments.</summary>
 		public static readonly String10 MSG_NET_DESPAWNED = new String10("NET_UNSPN");
+		/// <summary>Sent when a local player is setup (input is mapped to network player spawn). Data set to PlayerInput object.</summary>
 		public static readonly String10 MSG_PLAYER_SPAWN_LOCAL = new String10("DOSPAWN");
+		/// <summary>Sent when a local player input is lost or closed and the network player is despawned. NumArg set to the player input index.</summary>
 		public static readonly String10 MSG_PLAYER_LEFT_LOCAL = new String10("LOSTINPUT");
 		//public static readonly String10 MSG_NET_CHANGE = new String10("NET_CHG");
 
 		public static Context Inst;
 
 		public static readonly EventBus_NoAlloc Bus = new EventBus_NoAlloc();
-
-		//public Action<LocalNetworkPlayer> DoRegisterNetworkBridge;
 
 		void Awake()
 		{
